12ae987e126f972114c158a5d17bb599ba7973b0
unknown <susanveesa75@outlook.com>
2019-11-05 17:04:44 +0530

    some more examples using Eclipse

---
+++ javaFundamentals/src/javaFundamentals/ForLoop1.java
@@ -0,0 +1,12 @@
+package javaFundamentals;
+
+public class ForLoop1 {
+public static void main(String args[])
+{
+	for(int i=0;i<5;i++)
+	{
+		System.out.println("Hello  java " +i);
+		System.out.println("I have done....");
+	}
+}
+}
